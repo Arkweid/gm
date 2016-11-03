d0cf927dc5b373905bbfcf3d1b6524ed6ae568b9
Arkweid <a.a.abroskin@yandex.ru>
2016-11-03 14:39:14 +0300

    end characters prototype

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,14 +1,28 @@
 class CharactersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :load_character, only: [:update, :destroy]
+
+  respond_to :js
 
   def create
-    Character.create(character_params.merge(user: current_user))
-    redirect_to edit_user_registration_path
+    @character = Character.create(character_params.merge(user: current_user))
+  end
+
+  def update
+    @character.update(character_params)
+  end
+
+  def destroy
+    @character.destroy
   end
 
   private
 
   def character_params
     params.require(:character).permit(:game_id, :name, :description)
+  end
+
+  def load_character
+    @character = Character.find(params[:id])
   end
 end
--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe CharactersController, type: :controller do
-
 end
