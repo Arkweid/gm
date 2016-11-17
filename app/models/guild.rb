709c6f62303cc5fb38c4562a4c85b64ba7536fc4
Arkweid <a.a.abroskin@yandex.ru>
2016-11-17 10:59:04 +0300

    add default image

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -7,4 +7,8 @@ class Guild < ApplicationRecord
   validates :user_id, :game_id, :title, presence: true
 
   scope :search_guild, ->(search) { where('title LIKE ?', "%#{search}%") }
+
+  def get_emblem_link
+    emblem_link.blank? ? "guild_default_emblem.png" : emblem_link
+  end
 end
